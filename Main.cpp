5cfd789b4367c46a1413dcb5cd0b787f0fc2b8d7
Adder <adder_2003@yahoo.com>
2023-07-12 17:20:51 +0300

    `main`: Commands are case-insensitive. `Help`. Working on `AddSong`.

--- Main.cpp
+++ Main.cpp
@@ -10,7 +10,12 @@
 #include <iostream>
 #include <sstream>
 #include <iomanip>
+#include <locale>
+
+#include <algorithm>
+
 #include <string>
+
 
 #include <thread>
 #include <mutex>
@@ -20,6 +25,8 @@
 int main ()
 {
 	std::cout << "Hello, Imaginary World !\n" << std::flush;
+	const std::locale loc {std::cout.getloc ()};
+	const std::ctype <char> &ctype {std::use_facet <std::ctype <char>> (loc)};
 	
 	using namespace ImaginaryPlayer;
 	
@@ -40,10 +47,12 @@ int main ()
 		
 		ComposeAndLog (logcontext, [] (std::ostream &os) { os << "Surprise !\n"; });
 		
+		const auto songExample = Song {}.SetArtistName ("Madonna").SetSongName ("Frozen").SetCodecName ("MP3").SetLength (std::chrono::minutes {5} + std::chrono::seconds {29});
+		
 		for (;;)
 		{
 			ComposeAndLog (logcontext, [] (std::ostream &os) { os << "> "; });
-			std::string sLine;
+			std::string Command_sLine;
 			{
 				if (! std::cin)
 					{ ComposeAndLog (logcontext, [] (std::ostream &os) { std::cout << "cin had already failed !\n"; }); return 1; }
@@ -51,20 +60,95 @@ int main ()
 				if (std::cin.eof ())
 					{ ComposeAndLog (logcontext, [] (std::ostream &os) { std::cout << "cin has ended.\n"; }); break; }
 				
-				if (! std::getline (std::cin, sLine))
+				if (! std::getline (std::cin, Command_sLine))
 					{ ComposeAndLog (logcontext, [] (std::ostream &os) { std::cout << "getline has failed !\n"; }); return 1; }
 			}
 			
-			std::istringstream isLine {sLine};
+			std::istringstream Command_isLine {Command_sLine};
 			Cursor cursor;
-			const auto Command_opttoken {ExtractToken (isLine, &cursor)};
+			const auto Command_opttoken {ExtractToken (Command_isLine, &cursor)};
 			if (! Command_opttoken)
-				{ ComposeAndLog (logcontext, [&] (std::ostream &os) { os << "ExtractToken has failed for input \"" << sLine << "\" !\n"; }); continue; }
+			{
+				ComposeAndLog
+				(
+					logcontext,
+					[&] (std::ostream &os) { os << "ExtractToken has failed for input \"" << Command_sLine << "\" ! Please type \"help\" for help.\n"; }
+				);
+				
+				continue;
+			}
 			const auto &Command_token {*Command_opttoken};
 			const std::string Command_sText {Command_token.GetText ()};
+			      std::string Command_sTextLo {Command_sText};
+			{
+				std::transform
+				(
+					Command_sTextLo.begin (), Command_sTextLo.end (),
+					Command_sTextLo.begin (),
+					[&] (char c) { return ctype.tolower (c); }
+				);
+			}
 			
-			if (Command_sText == "exit")
+			if (Command_sTextLo == "help" || Command_sTextLo == "?")
+			{
+				std::ostringstream osHelp;
+				{
+					osHelp
+						<<
+							"Welcome to the ImaginaryPlayer !\n"
+							"\n"
+							"Commands (case-insensitive):\n"
+							"\n"
+							"    Help\n"
+							"        Displays this helpful message.\n"
+							"\n"
+							"    Exit\n"
+							"        Enqueues \"exit\" command to worker threads, then exits the main loop too.\n"
+							"        That is a nerdy way of saying that we exit the ImaginaryPlayer.\n"
+							"\n"
+							"    AddSong (<song-description>)\n"
+							"        Adds the specified song to the queue.\n"
+							"\n"
+							"    \n"
+							"        \n"
+							"\n"
+							"Example of <song-description>:\n"
+							"    " << songExample << "\n"
+							"\n"
+							"Contact/support: adder_2003@yahoo.com. Thank you soooo !! (-:\n"
+							"\n";
+				};
+				
+				ComposeAndLog (logcontext, [&] (std::ostream &os) { os << "\n" << osHelp.str (); });
+			}
+			else
+			if (Command_sTextLo == "exit")
 				break;
+			else
+			if (Command_sTextLo == "addsong")
+			{
+				Song song;
+				{
+					if (! (Command_isLine >> song))
+					{
+						ComposeAndLog
+						(
+							logcontext,
+							[&] (std::ostream &os)
+							{
+								os
+									<< "We have not been able to process the `Song` description !\n"
+									<< "Example of an expected `Song` description:\n"
+									<< "    " << songExample << "\n";
+							}
+						);
+						
+						continue;
+					}
+				}
+				
+				ComposeAndLog (logcontext, [&] (std::ostream &os) { os << "We are adding " << song << "...\n"; });
+			}
 			else
 				{ ComposeAndLog (logcontext, [&] (std::ostream &os) { os << "Unrecognized command: \"" << Command_sText << "\" !\n"; }); continue; }
 		}
