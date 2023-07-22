f1817ac54315adfcfedd0ddba1a7dc3f8e96d861
Adder <adder_2003@yahoo.com>
2023-07-22 06:10:38 +0300

    `Song_UT`: We have added a whitespace before the song description.
    This adds the testing of the ability to skip initial whitespace.

--- Song_UT.cpp
+++ Song_UT.cpp
@@ -7,11 +7,11 @@ int main ()
 {
 	using namespace ImaginaryPlayer;
 	
-	const char *const pszExpected {"(artist \"Madonna\", song \"Frozen\", codec \"MP3\", length 329000)"};
+	const char *const pszExpected {" (artist \"Madonna\", song \"Frozen\", codec \"MP3\", length 329000)"};
 	
 	{
 		const auto song = Song {}.SetArtistName ("Madonna").SetSongName ("Frozen").SetCodecName ("MP3").SetLength (std::chrono::minutes {5} + std::chrono::seconds {29});
-		std::ostringstream os; { os << song; }
+		std::ostringstream os; { os << " " << song; }
 		Azzert (os.str () == pszExpected);
 		std::cout << song << '\n';
 		
